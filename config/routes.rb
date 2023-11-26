d907c50a0712288471ab0877b7ea641fbf3a79cd
Ubuntu <ubuntu@ip-172-31-11-170.ap-northeast-1.compute.internal>
2023-11-26 15:56:24 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :microposts, only: [:create, :destroy]
+
 end
