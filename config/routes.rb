95b1210bf444760fa17f443fc269779d8189f051
Ubuntu <ubuntu@ip-172-31-11-170.ap-northeast-1.compute.internal>
2023-12-10 07:05:37 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,19 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy'   
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  member do
+    get :followings
+    get :followers
+   end
+    collection do
+      get :serach
+    end
+  end
+
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 end
